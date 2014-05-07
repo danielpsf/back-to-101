800d5929f79d71810cabc4751a80444ea72db1e3
danielpsf <danielpsf@gmail.com>
2014-05-07 12:54:24 -0300

    Adding a new test to check what structure is faster to sort: LikedList or ArrayList.

--- 1_lists_and_gqm/src/test/groovy/net/carlosgsouza/backto101/class01/LinkedAndArrayListPerformanceAnalysis.groovy
+++ 1_lists_and_gqm/src/test/groovy/net/carlosgsouza/backto101/class01/LinkedAndArrayListPerformanceAnalysis.groovy
@@ -63,4 +63,64 @@ public class LinkedAndArrayListPerformanceAnalysis extends Specification {
 		and: "answer the question"
 		linkedListMeasurements[MEASUREMENTS] < arrayListMeasurements[MEASUREMENTS]
 	}
+	
+	def "Is it faster to sort linkedList than arrayList"() {
+		given: "A linkedList and a arrayList"
+		List<Integer> arrayList
+		List<Integer> linkedList
+		
+		and: "the test parameters"
+		int ROUNDS = 10
+		int MEASUREMENTS = 100
+		int ITENS_PER_MEASUREMENT = 10000
+		Random random = new Random()
+		
+		and: "arrays to store the measurements"
+		int[] linkedListMeasurements = new int[MEASUREMENTS + 1]
+		int[] arrayListMeasurements = new int[MEASUREMENTS + 1]
+		
+		when: "measuring the performance of a linked list"
+		ROUNDS.times {
+			linkedList = new LinkedList<Integer>()
+			
+			MEASUREMENTS.times { i ->
+				ITENS_PER_MEASUREMENT.times {
+					linkedList.add(random.nextInt() * i)
+				}
+				
+				long start = System.currentTimeMillis()
+				linkedList.sort()
+				linkedListMeasurements[i] += System.currentTimeMillis() - start
+			}
+		}
+		
+		and: "measuring the performance of a array list"
+		ROUNDS.times {
+			arrayList = new ArrayList<Integer>()
+			
+			MEASUREMENTS.times { i ->
+				ITENS_PER_MEASUREMENT.times {
+					arrayList.add(random.nextInt() * i)
+				}
+				
+				long start = System.currentTimeMillis()
+				arrayList.sort()
+				arrayListMeasurements[i] += System.currentTimeMillis() - start
+			}
+		}
+		
+		then: "print the results"
+		
+		println "i\tLinked\tArray"
+		(MEASUREMENTS + 1).times {
+			def numberOfItems = it * ITENS_PER_MEASUREMENT
+			def linkedListTime = linkedListMeasurements[it] / ROUNDS
+			def arrayListTime = arrayListMeasurements[it] / ROUNDS
+			
+			println "$numberOfItems\t$linkedListTime\t$arrayListTime"
+		}
+		
+		and: "answer the question"
+		linkedListMeasurements[MEASUREMENTS] < arrayListMeasurements[MEASUREMENTS]
+	}
 }
